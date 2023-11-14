437532d0b6df5473e17542e6df5aca0b258d2e88
LcuasLedesma <lucasfidel99@gmail.com>
2023-11-14 10:47:01 -0300

    bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,25 @@
-import ModalProvider from "@/providers/modal-provider";
-import "./globals.css";
-import type { Metadata } from "next";
-import ToastProvider from "@/providers/toast-provider";
-import { ThemeProvider } from "@/providers/theme-provider";
+import ModalProvider from '@/providers/modal-provider'
+import './globals.css'
+import type { Metadata } from 'next'
+import ToastProvider from '@/providers/toast-provider'
 
 export const metadata: Metadata = {
-  title: "Nada Que Perder",
-  description: "NQP - Nada Que Perder",
-};
+	title: 'Nada Que Perder',
+	description: 'NQP - Nada Que Perder',
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <ThemeProvider defaultTheme="light" attribute="class" />
-      <ModalProvider />
-      <ToastProvider />
-      <body className="grainy">
-        <div>{children}</div>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<ModalProvider />
+			<ToastProvider />
+			<body className='grainy'>
+				<div>{children}</div>
+			</body>
+		</html>
+	)
 }
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,52 +1,49 @@
-"use client";
+'use client'
 
-import Image from "next/image";
-import { MouseEventHandler } from "react";
-import { Expand } from "lucide-react";
-import { useRouter } from "next/navigation";
-import IconButton from "@/components/ui/icon-button";
-import usePreviewModal from "@/hooks/use-preview-modal";
-import { Product } from "@/type";
+import Image from 'next/image'
+import { MouseEventHandler } from 'react'
+import { Expand } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import IconButton from '@/components/ui/icon-button'
+import usePreviewModal from '@/hooks/use-preview-modal'
+import { Product } from '@/type'
 
 interface ProductCard {
-  data: Product;
+	data: Product
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
-  const previewModal = usePreviewModal();
-  const router = useRouter();
+	const previewModal = usePreviewModal()
+	const router = useRouter()
 
-  const handleClick = () => {
-    router.push(`/product/${data?.id}`);
-  };
+	const onPreview: MouseEventHandler = (event) => {
+		event.stopPropagation()
 
-  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
-    event.stopPropagation();
+		previewModal.onOpen(data)
+	}
 
-    previewModal.onOpen(data);
-  };
+	return (
+		<div className='group cursor-pointer w-full relative'>
+			<div className='overflow-hidden'>
+				<Image
+					src={data.images?.[0]?.url}
+					alt='Porduct Image'
+					width={600}
+					height={400}
+					onClick={onPreview}
+					className=' object-cover w-full transition-all duration-1000 hover:scale-110 aspect-[3/4] '
+				/>
+			</div>
+			<div className='opacity-0 group-hover:opacity-100 transition absolute w-full bottom-5'>
+				<IconButton
+					onClick={onPreview}
+					icon={
+						<Expand size={20} className='text-gray-600' />
+					}
+				/>
+			</div>
+		</div>
+	)
+}
 
-  return (
-    <div className="group cursor-pointer w-full relative">
-      <div className="overflow-hidden">
-        <Image
-          src={data.images?.[0]?.url}
-          alt=""
-          width={600}
-          height={400}
-          className=" object-cover w-full transition-all duration-1000 hover:scale-110 aspect-[3/4] "
-          onClick={handleClick}
-          onLoad={(e) => console.log("aaaaaaaaaaaaaaa")}
-        />
-      </div>
-      <div className="opacity-0 group-hover:opacity-100 transition absolute w-full bottom-5">
-        <IconButton
-          onClick={onPreview}
-          icon={<Expand size={20} className="text-gray-600" />}
-        />
-      </div>
-    </div>
-  );
-};
-
-export default ProductCard;
+export default ProductCard

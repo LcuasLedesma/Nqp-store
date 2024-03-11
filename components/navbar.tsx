5ff1f3afc1742159832469a436d4c338feb7a932
luciano ledesma <lucasfidel99@gmail.com>
2024-03-11 06:19:33 -0300

    test navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,18 @@
 import Link from 'next/link'
 import logo from '../public/logo/logo.png'
 import Image from 'next/image'
+import getCategories from '@/actions/get-categories'
+import Route from './route'
+import MobileMenu from './ui/mobile-menu'
 
 export const revalidate = 0
 
 const Navbar = async () => {
-	// const categories = await getCategories();
+	const categories = await getCategories()
+
+	if (categories.length === 0) {
+		return
+	}
 
 	return (
 		<div className='mx-auto justify-center top-0 left-0 z-50 md:ml-10 md:mt-10 md:fixed w-[175px] '>
@@ -20,9 +27,9 @@ const Navbar = async () => {
 					/>
 				</Link>
 
-				{/* <Route categories={categories} />
+				<Route categories={categories} />
 
-        <MobileMenu categories={categories} /> */}
+				<MobileMenu categories={categories} />
 			</div>
 		</div>
 	)
